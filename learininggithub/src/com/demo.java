40e3fbcc2f61df5aa152eb8c1026c61a876cedb1
vaishnavipatil1107 <patilvaishnavirajendra@gmail.com>
2025-11-28 13:02:19 +0530

    Update demo.java

--- learininggithub/src/com/demo.java
+++ learininggithub/src/com/demo.java
@@ -2,15 +2,16 @@ package com;
 
 public class demo {
 	public void add(){
-		System.out.println(2+2)
+		System.out.println(2+2);
 			}
 	public static void main(String[] args) {
 		System.out.println("Learning github");
 		System.out.println("Change by me");
 
-		Demo d=new Demo();
+		demo d=new demo();
 		d.add();
 	}
 
 }
+
 
